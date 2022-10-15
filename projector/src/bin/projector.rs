635755e8ad18d2704c4151e89edd35feb46f1a18
Andre <33375224+Andree37@users.noreply.github.com>
2022-10-15 22:18:14 +0100

    wrapping up

--- projector/src/bin/projector.rs
+++ projector/src/bin/projector.rs
@@ -1,10 +1,35 @@
 use anyhow::Result;
 use clap::Parser;
+
 use projector::{config::Config, opts::Opts};
+use projector::config::Operation;
+use projector::projector::Projector;
 
 fn main() -> Result<()> {
-    let opts: Config = Opts::parse().try_into()?;
-    println!("{:?}", opts);
+    let config: Config = Opts::parse().try_into()?;
+    let mut proj: Projector = Projector::from_config(config.config, config.pwd);
+
+    match config.operation {
+        Operation::Print(None) => {
+            let value = proj.get_value_all();
+            let value = serde_json::to_string(&value)?;
+
+            println!("{}", value);
+        }
+        Operation::Print(Some(k)) => {
+            proj.get_value(&k).map(|x| {
+                println!("{}", x);
+            });
+        }
+        Operation::Add(k, v) => {
+            proj.set_value(k, v);
+            proj.save()?;
+        }
+        Operation::Remove(k) => {
+            proj.remove_value(&k);
+            proj.save()?;
+        }
+    }
 
     return Ok(());
 }
